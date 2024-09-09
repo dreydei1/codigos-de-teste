99588baba4e32911198892d561afd91fbbec4292
dreydei1 <163125170+dreydei1@users.noreply.github.com>
2024-09-09 12:34:28 -0300

    criei tudo a bagaça fi

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".mudaTextoBotao").addEventListener("click",function(){
+    const paragrafo = document.querySelector(".descricao");
+    paragrafo.textContent = "texto alterado!";
+});
+
+document.querySelector(".destacarBotao").addEventListener("click", function(){
+    const titulo = document.querySelector(".titulo");
+    titulo.classList.add("destaque");
+});
