36b05c1c14f37dad55ab15ab21fec66713e6c6d2
dreydei1 <163125170+dreydei1@users.noreply.github.com>
2024-09-12 11:13:40 -0300

    criei

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelector(".mudarTextoBotao").addEventListener("click",function(){
-    const paragrafo = document.querySelector(".descricao");
-    paragrafo.textContent = "texto alterado!";
+document.querySelector('.mudarTextoBotao').addEventListener('click',function(){
+    const paragrafo = document.querySelector('.descricao');
+    paragrafo.textContent = 'texto alterado!';
 });
 
-document.querySelector(".destacarBotao").addEventListener("click", function(){
-    const titulo = document.querySelector(".titulo");
-    titulo.classList.add("destaque");
+document.querySelector('.destacarBotao').addEventListener('lick', function(){
+    const titulo = document.querySelector('.titulo');
+    titulo.classList.add('destaque');
 });
